4f7d0378391776204ddc67e0efaed42a2489b867
liangmuren <2014302580143@whu.edu.cn>
2018-03-07 17:32:32 +0800

    add eslint and modify sendByServerChan

--- sendByServerChan.js
+++ sendByServerChan.js
@@ -1,12 +1,17 @@
 let request = require('request')
 const url = 'https://sc.ftqq.com/SCU17645Tad0bd9ce2c5f29468b1448d7af5ecb8a5a29537708e45.send'
-let send = function(text,desp){
-    let post_data ={
+let send = function (text, desp) {
+    let postData = {
         text,
         desp
     }
-    request.post({url:url,form:post_data},function(err,response,body){
-
+    request.post({
+        url: url,
+        form: postData
+    }, function (err) {
+        if (err) {
+            console.log(err)
+        }
     })
 }
 module.exports = send
